56dc1a4032ac6434aea0e7cdd671465b7561b949
Tim Olson <>
2023-11-20 21:23:14 -0400

    deploy doesnt change k8s files

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,31 @@
 #!/usr/bin/env bash
+
+if [ "$1" == "" ]; then
+  echo tag required
+  echo $0 [tag]
+fi
+
 ./bin/build.sh || exit 1
-PRIVATE_KEY=${PRIVATE_KEY:='0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}
-forge script script/Deploy.sol -vvvv --fork-url http://localhost:8545 --broadcast
+
+TAG=$1
+PRIVKEY=${PRIVATE_KEY:='0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'}
+RPC=${RPCURL:-http://localhost:8545}
+CHAINID=$(cast chain-id --rpc-url "$RPC")
+
+cpbroadcast() {
+  CONTRACT=$1
+  mkdir -p deployment/"$TAG"/broadcast/"$CONTRACT"/"$CHAINID"
+  cp broadcast/"$CONTRACT"/"$CHAINID"/run-latest.json deployment/"$TAG"/broadcast/"$CONTRACT"/"$CHAINID"/run-"$TAG".json
+}
+
+PRIVATE_KEY=$PRIVKEY forge script script/Deploy.sol -vvvv --fork-url "$RPC" --broadcast
+rm -rf deployment/"$TAG"
+mkdir -p deployment/"$TAG"
+cp -r out deployment/"$TAG"/
+cpbroadcast Deploy.sol
+
+if [ "$2" == "mock" ]; then
+  PRIVATE_KEY=$PRIVKEY forge script script/DeployMock.sol -vvvv --fork-url "$RPC" --broadcast
+  cpbroadcast DeployMock.sol
+fi
+
